ed7d55e880b29c543691283182c83317f0076c65
Minh Quang <quangduong.1598@gmail.com>
2021-08-20 15:27:48 +0700

    first commit

---
+++ app/src/main/java/com/example/appmusic/adapter/SongAdapter.java
@@ -0,0 +1,96 @@
+package com.example.appmusic.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.appmusic.ui.ItemClick;
+import com.example.appmusic.model.Song;
+import com.example.appmusic.R;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
+    ArrayList<Song> songArrayList;
+    Context context;
+    private ItemClick itemClick;
+
+    public SongAdapter(ArrayList<Song> songArrayList , ItemClick itemClick) {
+        this.songArrayList = songArrayList;
+        this.itemClick = itemClick;
+    }
+
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View itemview = layoutInflater.inflate(R.layout.song_iteam,parent,false);
+        return new ViewHolder(itemview);
+    }
+
+    @Override
+    public void onBindViewHolder( SongAdapter.ViewHolder holder, int position) {
+        holder.txtId.setText(songArrayList.get(position).getmId());
+        holder.txtName.setText(songArrayList.get(position).getmName());
+        holder.txtTime.setText(songArrayList.get(position).getmTime());
+        TextView sName = holder.txtName;
+        TextView sTime = holder.txtTime;
+        holder.SetOnClickListener(new ItemClick(){
+
+            @Override
+            public void onItemClick(View view, int vitri) {
+
+            }
+        });
+
+
+
+    }
+
+    public String martchTime(long giay){
+        String time =String.format(String.valueOf(TimeUnit.MILLISECONDS.toMinutes(giay)),TimeUnit.MILLISECONDS.toSeconds(giay)
+         - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(giay)));
+        return  time;
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return songArrayList.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView txtId;
+        TextView txtName;
+        TextView txtTime;
+        private Song song;
+        ItemClick itemClick = null;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            txtId = itemView.findViewById(R.id.text_id);
+            txtName = itemView.findViewById(R.id.text_name);
+            txtTime = itemView.findViewById(R.id.text_time);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    itemClick.onItemClick(itemView,getAdapterPosition());
+                }
+            });
+        }
+        public final void SetOnClickListener(ItemClick clickitem){
+            this.itemClick = clickitem;
+        }
+
+
+        @Override
+        public void onClick(View v) {
+            itemClick.onItemClick(v,getAdapterPosition());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/appmusic/model/Song.java
@@ -0,0 +1,56 @@
+package com.example.appmusic.model;
+
+public class Song {
+    private String mName;
+    private String mTime;
+    private String mPath;
+    private int mId;
+
+    public Song(String mName, String mTime, String mPath, int mId) {
+        this.mName = mName;
+        this.mTime = mTime;
+        this.mPath = mPath;
+        this.mId = mId;
+    }
+    public Song(String mName, String mTime, String mPath) {
+        this.mName = mName;
+        this.mTime = mTime;
+        this.mPath = mPath;
+    }
+
+    public Song() {
+
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmTime() {
+        return mTime;
+    }
+
+    public void setmTime(String mTime) {
+        this.mTime = mTime;
+    }
+
+    public String getmPath() {
+        return mPath;
+    }
+
+    public void setmPath(String mPath) {
+        this.mPath = mPath;
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+}
